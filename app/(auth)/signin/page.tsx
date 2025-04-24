625988ac2ae3c3e5c6d4d8a1f739db45f23f0149
navjyot <navjyot.mishra.1@gmail.com>
2025-04-24 16:55:21 +0530

    first nextjs project

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
 import { supabase } from "@/library/supabaseClient";
 
 export default function SignIn() {
