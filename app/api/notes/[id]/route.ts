22770283721ab6b924b81983ef91cc9a10a2c678
navjyot <navjyot.mishra.1@gmail.com>
2025-04-25 13:21:36 +0530

    some minor fixes

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = params.id;
+  const { id } = await params;
 
   const userId = req.headers.get("x-user-id");
 
@@ -25,9 +25,9 @@ export async function DELETE(
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = params.id;
+  const { id } = await params;
 
   const { title, content, tag } = await req.json();
   const userId = req.headers.get("x-user-id");
